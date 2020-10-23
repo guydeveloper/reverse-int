66f0f1b4130d94b73850492656f7d5bac006798e
guydeveloper <guydeveloper@yandex.ru>
2020-10-23 21:34:09 +0500

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    let nString = n.toString();
+
+    for (let i = nString.length - 1; i >= 0; i -= 1) {
+        result += nString[i];
+    }
+    
+    return (n >= 0) ? result : result.slice(0, -1);
 }
